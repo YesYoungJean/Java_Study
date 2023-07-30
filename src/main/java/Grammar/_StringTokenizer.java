462f8e8fb7e0a809b5b321fb387ae759872802e4
develop-yjnoh <114342669+develop-yjnoh@users.noreply.github.com>
2023-07-30 17:52:25 +0900

    StringTokenizer test

---
+++ src/main/java/Grammar/_StringTokenizer.java
@@ -0,0 +1,30 @@
+package Grammar;
+
+import java.util.StringTokenizer;
+public class _StringTokenizer {
+
+    public static void main(String[] args) {
+        /**
+         * Topic: StringTokenizer
+         * >>>>>: 구분자를 이용하여 문자열을 분리하는 클래스
+         * @ 2023-07-30
+         * */
+
+        //분리 대상 String
+        String str = "손흥민!이강인!김민재";
+        String sep = "!";
+        StringTokenizer st1 = new StringTokenizer(str, sep);
+        StringTokenizer st2 = new StringTokenizer(str, sep, true);
+
+        System.out.println("-----sep not included-----");
+        while(st1.hasMoreTokens()){
+            System.out.println(st1.nextToken());
+        }//while
+
+        System.out.println("-----sep included-----");
+        while(st2.hasMoreTokens()){
+            System.out.println(st2.nextToken());
+        }//while
+
+    }//main end
+}
